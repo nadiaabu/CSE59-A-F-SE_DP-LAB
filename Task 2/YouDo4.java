128e4439f94019c49df91357ff69037334b519d3
Nadia Abu <nadiaabu2003@gmail.com>
2026-06-09 10:56:39 +0600

    Add Sensor class with charging functionality

--- Task 2/YouDo4.java
+++ Task 2/YouDo4.java
@@ -1,1 +1,21 @@
+class Sensor{
+    String name;
+    int batteryLevel;
+    
+    public void charge(){
+        batteryLevel= 100;
+        System.out.println("Sensor fully charged");
+        
+    }
+    
+  
+}
 
+public class Main {
+    public static void main (String [] args){
+        Sensor mySensor = new Sensor ();
+        mySensor.name = "Temperature Sensor";
+        mySensor.charge();
+        System.out.println("New battery level:" + mySensor.batteryLevel);
+    }
+}
