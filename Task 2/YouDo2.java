5f077f164355e8da42e9beba02d97c5fd7ff4cfc
Nadia Abu <nadiaabu2003@gmail.com>
2026-06-09 10:35:01 +0600

    Add NumberChecker class to check even numbers

--- Task 2/YouDo2.java
+++ Task 2/YouDo2.java
@@ -1,1 +1,18 @@
+class NumberChecker{
+    void check(int num){
+        if (( num % 2 == 0 ) && (num > 10 )){
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+            
+        } 
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        NumberChecker checker = new NumberChecker();
+        checker.check (14);
+    }
+}
 
