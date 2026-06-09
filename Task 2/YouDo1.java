12018e6df8c85a44bd786fa1e93293eb80da643c
Nadia Abu <nadiaabu2003@gmail.com>
2026-06-09 10:17:48 +0600

    Add ContestStats class to print first score

--- Task 2/YouDo1.java
+++ Task 2/YouDo1.java
@@ -1,1 +1,16 @@
+class ContestStats{
+    
+    int[] scores = {85,90,95};
+    public void printFirstScores(){
+        System.out.println("The first scores is : " + scores [0] );
+        
+    }
+    
+}
 
+public class main {
+    public static void main(String[] args){
+        ContestStats state = new ContestStats();
+        state.printFirstScores();
+    }
+}
