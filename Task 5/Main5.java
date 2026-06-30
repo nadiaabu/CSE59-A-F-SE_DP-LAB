c97a335aeb91e3fe834c035bb5fe5d013f17050c
Nadia Abu <nadiaabu2003@gmail.com>
2026-06-30 11:27:30 +0600

    Implement Researcher interface and GraduateResearchAssistant class
    Added interface Researcher and GraduateResearchAssistant class implementing it.

--- Task 5/Main5.java
+++ Task 5/Main5.java
@@ -1,1 +1,30 @@
+interface Researcher {
+    void conductLabResearch ();
+}
 
+class UniversityMember {
+    public void attendLecture (){
+        System.out.println ("Attending a Lecture.");
+    }
+}
+
+class UndergraduateStudent extends UniversityMember {
+    
+    
+}
+
+class GraduateResearchAssistend extends UniversityMember implements Researcher {
+    @Override 
+    public void conductLabResearch (){
+        System.out.println ("Publishing papers and running experiments.");
+    }
+}
+
+public static void main(String[] args) {
+        UniversityMember undergrad = new UndergraduateStudent();
+        undergrad.attendLecture();
+
+        GraduateResearchAssistend gra = new GraduateResearchAssistend();
+        gra.attendLecture();
+        gra.conductLabResearch();
+    }
