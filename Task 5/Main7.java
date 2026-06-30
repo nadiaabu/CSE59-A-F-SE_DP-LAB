37ebacaf946001bec467e4a7707dea54942ef154
Nadia Abu <nadiaabu2003@gmail.com>
2026-06-30 11:26:25 +0600

    Implement Bluetooth mesh network for alerts

--- Task 5/Main7.java
+++ Task 5/Main7.java
@@ -1,1 +1,31 @@
+interface INetworkProtocol {
+    void broadcastMessage (String msg);
+}
 
+class BluetoothMeshNetwork implements INetworkProtocol{
+    @Override 
+    public void broadcastMessage (String msg){
+        System.out.println("Broadcasting via Bluetooth Mesh: "+ msg);
+    }
+}
+
+class DisasterAlertSystem {
+    private final INetworkProtocol network ;
+    
+    public DisasterAlertSystem(INetworkProtocol network){
+        this.network = network;
+    }
+    
+    public void triggerAlert(String AlertData){
+        network.broadcastMessage(AlertData);
+    }
+}
+
+public class BasicPrinter {
+    public static void main (String[] args){
+        INetworkProtocol bluetooth = new BluetoothMeshNetwork();
+        DisasterAlertSystem System = new DisasterAlertSystem (bluetooth);
+        System.triggerAlert("Cyclone Warning ");
+    }
+    
+}
